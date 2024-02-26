01554fb8777537fa77d79213a54f2cbbbd55b361
Nicolas Ruiz <nicolas.ruiz2075@gmail.com>
2024-02-26 09:17:53 +0100

    Fin Tp4

--- src/main/java/HelloJava.java
+++ src/main/java/HelloJava.java
@@ -3,8 +3,15 @@
  * @author JMB
  * @author LC
  */
-class HelloJava {
-	public afficherCancan(){
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Je m'appelle Nicolas Ruiz");
+	}
+
+	public static void afficherCancan(){
+
+		System.out.println("Cancan");
 
 	}
 }
